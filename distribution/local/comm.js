11bb3e568a28821370c3a2ab5537ad615b3dce3f
angelaaaliii <angelali2021@gmail.com>
2025-02-28 21:02:43 -0500

    gossip

--- distribution/local/comm.js
+++ distribution/local/comm.js
@@ -1,6 +1,9 @@
 /** @typedef {import("../types").Callback} Callback */
 /** @typedef {import("../types").Node} Node */
 
+const { serialize } = require("../util/serialization");
+const { deserialize } = require("../util/serialization");
+const http = require('node:http');
 
 
 /**
@@ -16,7 +19,42 @@
  * @param {Callback} [callback]
  * @return {void}
  */
-function send(message, remote, callback) {
+function send(message=[], remote={node: "default", method: "default", service: "default"}, callback=(e, v)=>{}) {  
+  if (remote.node == "default") {
+    callback(new Error("no remote passed in"), null);
+    return;
+  }
+  let gid = "local"
+  if ('gid' in remote) {
+    gid = remote['gid'];
+  }
+  const serialized_msg = serialize(message);
+  const options = {
+    hostname: remote.node.ip,
+    port: remote.node.port,
+    path: '/' + gid + '/' + remote.service + '/' + remote.method,
+    method: 'PUT',
+  };
+
+  const req = http.request(options, (res) => {
+    let responseBody = '';
+
+    res.on('data', (chunk) => {
+        responseBody += chunk;
+    });
+
+    res.on('end', () => {
+      let deserialized_res = deserialize(responseBody);
+      return callback(...deserialized_res);
+    });
+  });
+
+  req.write(serialized_msg);
+  req.on('error', (err) => 
+    callback(new Error("comm send err", {source:err}), null)
+  // callback(err, null)
+);
+  req.end();
 }
 
-module.exports = require('@brown-ds/distribution/distribution/local/comm');
+module.exports = {send};
--- distribution/local/gossip.js
+++ distribution/local/gossip.js
@@ -1,7 +1,16 @@
+
 const gossip = {};
+const seenMessages = new Set();
 
-
-gossip.recv = function(payload, callback) {
+gossip.recv = function(payload="", callback=(e, v)=>{}) {
+  if (payload in seenMessages) {
+    callback(null, payload);
+    return;
+  } 
+  seenMessages.add(payload);
+  global.distribution.all.gossip.send(payload, remote, (e, v) => {
+    callback(e, v);
+  })
 };
 
 module.exports = gossip;
