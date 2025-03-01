9dcf3afd5aff53754c46c89d555c54f6e8860b4a
angelaaaliii <angelali2021@gmail.com>
2025-02-28 21:03:26 -0500

    mem

--- distribution/local/groups.js
+++ distribution/local/groups.js
@@ -1,19 +1,85 @@
+const { id } = require('../util/util');
 
-const groups = {};
+const sid = id.getSID(global.nodeConfig); // TODO? how to populate all ahead of time? if putting only when groups.put is called, doesn't that miss other potential nodes?
+const groups = {'all': {sid: global.nodeConfig}};
 
-groups.get = function(name, callback) {
+groups.get = function(name="", callback=(e, v)=>{}) {
+  if (name in groups) {
+    callback(null, groups[name]);
+  } else {
+    callback(new Error("could not find name in groups"), null);
+  }
 };
 
-groups.put = function(config, group, callback) {
+groups.put = function(config="", group={}, callback=(e, v)=>{}) {
+  let hash = null;
+  if (typeof(config) === 'object' && 'gid' in config){
+    if ('hash' in config) {
+      hash = config.hash;
+    }
+
+    config = config['gid'];
+  }
+  groups[config] = group;
+
+  // // loop through nodes in group and put into all if not already there:
+  // for (let k of Object.keys(group)) {
+  //   if (!(k in groups.all)) {
+  //     groups.all[k] = group[k];
+  //   }
+  // }
+
+  global.distribution[config] = {};
+  global.distribution[config].status =
+      require('../all/status')({gid: config});
+      global.distribution[config].comm =
+      require('../all/comm')({gid: config});
+      global.distribution[config].gossip =
+      require('../all/gossip')({gid: config});
+      global.distribution[config].groups =
+      require('../all/groups')({gid: config});
+      global.distribution[config].routes =
+      require('../all/routes')({gid: config});
+      global.distribution[config].mem =
+      require('../all/mem')({gid: config, hash: hash});
+      global.distribution[config].store =
+      require('../all/store')({gid: config, hash: hash});
+  callback(null, group);
 };
 
-groups.del = function(name, callback) {
+groups.del = function(name="", callback=(e, v) => {}) {
+  if (name in groups) {
+    const deleted = groups[name];
+    delete groups[name];
+    callback(null, deleted);
+  } else {
+    callback(new Error("no name in groups to delete"), null);
+  }
 };
 
-groups.add = function(name, node, callback) {
+groups.add = function(name="", node={}, callback=(e, v)=>{}) {
+  if (name in groups) {
+    groups[name][id.getSID(node)] = node;
+    callback(null, node);
+  } else {
+    callback(new Error("name not found"), null);
+  }
 };
 
-groups.rem = function(name, node, callback) {
+groups.rem = function(name="", node="", callback=(e, v)=>{}) {
+  if (!(name in groups)) {
+    callback(new Error("group name not found"), null);
+    return;
+  }
+  if (!(node in groups[name])) {
+    callback(null, groups[name]);
+    return;
+  }
+  delete groups[name][node];
+  if (groups[name] == {}) {
+    delete groups[name];
+  }
+  callback(null, groups);
 };
 
-module.exports = require('@brown-ds/distribution/distribution/local/groups');
+module.exports = groups;
--- distribution/local/mem.js
+++ distribution/local/mem.js
@@ -1,11 +1,74 @@
+const { id } = require("../util/util");
+
+const memMap = {};
+// memMap = {gid: {key: val, k: v}, gid: {key: val}}
 
 function put(state, configuration, callback) {
+  // normalizing config input
+  if (configuration === null) {
+    configuration = {key: id.getID(state), gid: "local"};
+  }
+  if (typeof(configuration) === 'string') {
+    configuration = {key: configuration, gid: "local"};
+  }
+  if (!("key" in configuration)) {
+    // config is map, check if key is null
+    configuration.key = id.getID(state);
+    configuration.gid = configuration.gid || 'local';
+  }
+  if ('key' in configuration && configuration.key === null) {
+    configuration.key = id.getID(state);
+    configuration.gid = configuration.gid || 'local';
+  }
+  // adding/updating map
+  if (!(configuration.gid in memMap)) {
+    memMap[configuration['gid']] = {};
+  } 
+  memMap[configuration['gid']][configuration['key']] = state;
+
+  callback(null, state);
+  return;
 };
 
 function get(configuration, callback) {
+  if (configuration === null) {
+    configuration = {key: null, gid: "local"};
+  }
+  if (typeof(configuration) == 'string') {
+    configuration = {key: configuration, gid: "local"};
+  } 
+  if (configuration.key === null) {
+    if (configuration.gid in memMap) {
+      callback(null, Object.keys(memMap[configuration.gid]));
+    } else {
+      callback(null, []);
+    }
+    return;
+  }
+  if (!('gid' in configuration)) {
+    configuration.gid = 'local';
+  }
+
+  if (configuration.gid in memMap && configuration.key in memMap[configuration.gid]) {
+    callback(null, memMap[configuration.gid][configuration.key]);
+    return;
+  }
+  callback(new Error("key not found in in-mem"), null);
 }
 
 function del(configuration, callback) {
+  if (typeof(configuration) === 'string') {
+    configuration = {key: configuration, gid: "local"};
+  }
+
+  if (configuration.gid in memMap && configuration.key in memMap[configuration.gid]) {
+    const val = memMap[configuration.gid][configuration.key];
+    delete memMap[configuration.gid][configuration.key];
+    callback(null, val);
+    return;
+  }
+
+  callback(new Error("mem del - key not found in in-mem"), null);
 };
 
 module.exports = {put, get, del};
