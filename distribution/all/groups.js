d90649e29c70944325c8971e408f6d49220414e6
angelaaaliii <angelali2021@gmail.com>
2025-02-28 20:41:28 -0500

    a

--- distribution/all/groups.js
+++ distribution/all/groups.js
@@ -4,18 +4,43 @@ const groups = function(config) {
 
   return {
     put: (config, group, callback) => {
+      const remote = {service: 'groups', method: 'put'};
+      global.distribution[context.gid].comm.send([config, group], remote, (e, v)=> {
+        callback(e, v);
+        return;
+      });
     },
 
     del: (name, callback) => {
+      const remote = {service: 'groups', method: 'del'};
+      global.distribution[context.gid].comm.send([name], remote, (e, v)=> {
+        callback(e, v);
+        return;
+      });
     },
 
     get: (name, callback) => {
+      const remote = {service: 'groups', method: 'get'};
+      global.distribution[context.gid].comm.send([name], remote, (e, v)=> {
+        callback(e, v);
+        return;
+      });
     },
 
     add: (name, node, callback) => {
+      const remote = {service: 'groups', method: 'add'};
+      global.distribution[context.gid].comm.send([name, node], remote, (e, v)=> {
+        callback(e, v);
+        return;
+      });
     },
 
     rem: (name, node, callback) => {
+      const remote = {service: 'groups', method: 'rem'};
+      global.distribution[context.gid].comm.send([name, node], remote, (e, v)=> {
+        callback(e, v);
+        return;
+      });
     },
   };
 };
