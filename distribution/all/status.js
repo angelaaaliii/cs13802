07d178c251cafcf18dc19dea353ce3865fa8462e
angelaaaliii <angelali2021@gmail.com>
2025-02-28 21:01:11 -0500

    all

--- distribution/all/status.js
+++ distribution/all/status.js
@@ -1,3 +1,4 @@
+const { id } = require("../util/util");
 
 const status = function(config) {
   const context = {};
@@ -5,14 +6,89 @@ const status = function(config) {
 
   return {
     get: (configuration, callback) => {
+      const remote = {service: 'status', method: 'get'};
+      global.distribution[context.gid].comm.send([configuration], remote, (e, v)=> {
+        let v2 = 0;
+        if (configuration == 'counts' || configuration == 'heapTotal' || configuration == 'heapUsed') {
+          // must sum
+          for (let key in v) {
+            v2 += v[key];
+          }
+          callback(e, v2);
+        } else {
+          // no need to sum
+          callback(e, v);
+        }
+        return;
+      });
     },
 
     spawn: (configuration, callback) => {
+      // The spawn method starts a new node with appropriate IP and port information, 
+      // and adds that node to the corresponding group for all nodes (see above)
+
+      global.distribution.local.status.spawn(configuration, (e, v) => {
+        if (e) {
+          callback(e, null);
+          return;
+        }
+
+        global.distribution.local.groups.add(context.gid, configuration, (e, v)=> {
+          callback(null, v);
+        });
+        
+        // adds node to corresponding group for all nodes
+        const remote = {service: 'groups', method: 'add'};
+        global.distribution[context.gid].comm.send([context.gid, v], remote, (e, v) => {
+        });
+
+      });
     },
 
     stop: (callback) => {
+      let val_map = {};
+      let err_map = {};
+      let group_nodes;
+      
+      global.distribution.local.groups.get(context.gid, (e, v) => {
+        if (e) {
+          callback(e, null);
+          return;
+        }
+        group_nodes = v;
+        const group_len = Object.keys(v).length;
+
+        let i = 0;
+        for (let sid in group_nodes) { // TODO use nid
+          const nid = id.getNID(group_nodes[sid]);
+          if (nid != global.nodeConfig.nid) {
+            // stop nodes that are not local node
+            let configuration = {node: group_nodes[sid], method: "stop", service: 'status'};
+
+            global.distribution.local.comm.send([], configuration, (e, v) => {
+              if (e) {
+                err_map[sid] = e;
+              } else {
+                val_map[sid] = v;
+              }
+
+              i += 1;
+              if (i == group_len ) {
+                callback(err_map, val_map);
+                return;
+              }
+            })
+          } else {
+            i += 1;
+            if (i == group_len ) {
+              callback(err_map, val_map);
+              return;
+            }
+          }
+        }
+      });
     },
   };
 };
 
-module.exports = require('@brown-ds/distribution/distribution/all/status');
+module.exports = status;
--- distribution/all/store.js
+++ distribution/all/store.js
@@ -1,3 +1,4 @@
+const { id } = require("../util/util");
 
 function store(config) {
   const context = {};
@@ -8,15 +9,176 @@ function store(config) {
           always be a string */
   return {
     get: (configuration, callback) => {
+      if (configuration === null) {
+        const remote = {service: "store", method: "get"};
+        const message = [{key: null, gid: context.gid}];
+        global.distribution[context.gid].comm.send(message, remote, (e, v) => {
+          let keys = [];
+          for (let val of Object.values(v)) {
+            keys = [...keys, ...val];
+          }
+          callback(e, keys);
+          return;
+        });
+      } else {
+        let kid = id.getID(configuration);
+
+        global.distribution.local.groups.get(context.gid, (e, v) => {
+          // map from nid to node
+          const nidToNode = {};
+          for (const n of Object.values(v)) {
+            nidToNode[id.getNID(n)] = n;
+          }
+          const nids = Object.keys(nidToNode);
+          const nid = context.hash(kid, nids);
+          const remote = {service: "store", method: "get", node: nidToNode[nid]};
+          const message = [{key: configuration, gid: context.gid}];
+          global.distribution.local.comm.send(message, remote, (e, v) => {
+            if (e) {
+              callback(e, null);
+            } else {
+              callback(null, v);
+            }
+          });
+        });
+      }
     },
 
     put: (state, configuration, callback) => {
+      let kid = id.getID(configuration);
+      if (configuration == null) {
+        kid = id.getID(id.getID(state));
+      }
+
+      global.distribution.local.groups.get(context.gid, (e, v) => {
+        // map from nid to node
+        const nidToNode = {};
+        for (const n of Object.values(v)) {
+          nidToNode[id.getNID(n)] = n;
+        }
+        const nids = Object.keys(nidToNode);
+        const nid = context.hash(kid, nids);
+        const remote = {service: "store", method: "put", node: nidToNode[nid]};
+        const message = [state, {key: configuration, gid: context.gid}];
+        global.distribution.local.comm.send(message, remote, (e, v) => {
+          if (e) {
+            callback(e, null);
+          } else {
+            callback(null, v);
+          }
+        });
+      });
     },
 
     del: (configuration, callback) => {
+      let kid = id.getID(configuration);
+
+      global.distribution.local.groups.get(context.gid, (e, v) => {
+        // map from nid to node
+        const nidToNode = {};
+        for (const n of Object.values(v)) {
+          nidToNode[id.getNID(n)] = n;
+        }
+        const nids = Object.keys(nidToNode);
+        const nid = context.hash(kid, nids);
+        const remote = {service: "store", method: "del", node: nidToNode[nid]};
+        const message = [{key: configuration, gid: context.gid}];
+        global.distribution.local.comm.send(message, remote, (e, v) => {
+          if (e) {
+            callback(e, null);
+          } else {
+            callback(null, v);
+          }
+        });
+      });
     },
 
     reconf: (configuration, callback) => {
+      // getting all keys
+      global.distribution[context.gid].store.get(null, (e, v) => {
+        if (Object.keys(e).length === 0) {
+          const allKeys = v; // all keys
+          let keysToRelocate = {}; // {key: {oldNode: x, newNode: y}}
+
+          const oldNodes = {}; // {nid: node}
+          const newNodes = {}; // {nid: node}
+
+          global.distribution.local.groups.get(context.gid, (e, v) => {
+            if (e) {
+              callback(e, null);
+              return;
+            }
+
+            // populating old nodes
+            for (const node of Object.values(configuration)) {
+              oldNodes[id.getNID(node)] = node;
+            }
+
+            // populating new nodes
+            for (const node of Object.values(v)) {
+              newNodes[id.getNID(node)] = node;
+            }
+
+            // loop through all keys and rehash given old group and new group nids
+            for (const k of allKeys) {
+              const oldNid = context.hash(id.getID(k), Object.keys(oldNodes));
+              const newNid = context.hash(id.getID(k), Object.keys(newNodes));
+
+              if (oldNid != newNid) {
+                // if diff, must be reconf so add to relocation map
+                keysToRelocate[k] = {oldNode: oldNodes[oldNid], newNode: newNodes[newNid]};
+              }
+            }
+
+            let numRelocated = 0;
+            // loop through all keys to relocate
+            for (const key of Object.keys(keysToRelocate)) {
+              // get from old node to get object associated with key
+              let message = [{key: key, gid: context.gid}];
+              let remote = {method: "get", service: "store", node: keysToRelocate[key].oldNode};
+              global.distribution.local.comm.send(message, remote, (e, v) => {
+                if (e) {
+                  callback(e, null);
+                  return;
+                }
+                const obj = v;
+
+                // delete from old node
+                message = [{key: key, gid: context.gid}];
+                remote = {node: keysToRelocate[key].oldNode, method: "del", service: "store"};
+                global.distribution.local.comm.send(message, remote, (e, v) => {
+                  if (e) {
+                    callback(e, null);
+                    return;
+                  }
+
+                  // put on new node
+                  message = [obj, {key: key, gid: context.gid}];
+                  remote = {node: keysToRelocate[key].newNode, method: "put", service: "store"};
+                  global.distribution.local.comm.send(message, remote, (e, v) => {
+                    if (e) {
+                      callback(e, null);
+                      return;
+                    }
+
+                    numRelocated++;
+                    if (numRelocated === Object.keys(keysToRelocate).length) {
+                      callback(null, v);
+                      return;
+                    }
+
+                  });
+                });
+              });
+            }
+            
+          });
+        
+        } else {
+          callback(e, v);
+          return;
+        }
+      });
     },
   };
 };
